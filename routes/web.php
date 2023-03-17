5e925da8bb310d08e3d879433a8708b2c4f28046
maarquimdoido <mv_bcardoso@outlook.com>
2023-03-17 15:21:55 +0000

    More Editing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +21,43 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:user'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'role:admin'])->group(function(){
+    Route::controller(DashboardController::class)->group(function(){
+        Route::get('/admin/dashboard', 'Index');
+    });
+    
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'Index')->name('allcategory');
+        Route::get('/admin/ad-category', 'AddCategory')->name('addcategory');
+    });
+
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'Index')->name('allcategory');
+        Route::get('/admin/ad-category', 'AddCategory')->name('addcategory');
+    });
+
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'Index')->name('allcategory');
+        Route::get('/admin/ad-category', 'AddCategory')->name('addcategory');
+    });
+
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'Index')->name('allcategory');
+        Route::get('/admin/ad-category', 'AddCategory')->name('addcategory');
+    });
+        
+        
+});
+
+
+
 
 require __DIR__.'/auth.php';
