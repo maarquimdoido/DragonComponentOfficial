0912c9a02f46a767729c3aec54c8c4e9d156a75e
maarquimdoido <mv_bcardoso@outlook.com>
2023-03-18 02:03:45 +0000

    retirei dropdown das pages AllCategories etc ..

--- resources/views/admin/allproducts.blade.php
+++ resources/views/admin/allproducts.blade.php
@@ -31,9 +31,6 @@ All Products - Dragon Component
                       </tr>
                       <tr>
                           <div class="dropdown">
-                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i class="bx bx-dots-vertical-rounded"></i>
-                            </button>
                             <div class="dropdown-menu">
                               <a class="dropdown-item" href="javascript:void(0);"
                                 ><i class="bx bx-edit-alt me-1"></i> Edit</a
