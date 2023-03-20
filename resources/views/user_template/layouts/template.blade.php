3090e17be2fe11e129dc2a1694dda11502827482
Davi Neves <tio.sam989@hotmail.com>
2023-03-20 01:57:50 +0000

    aa

--- resources/views/user_template/layouts/template.blade.php
+++ resources/views/user_template/layouts/template.blade.php
@@ -13,7 +13,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>Eflyer</title>
+      <title>Dragon Component</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
