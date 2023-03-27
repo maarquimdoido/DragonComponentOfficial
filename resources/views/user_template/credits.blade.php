682a6b4d879cff2e46fa1b08ca416ad3a52c7ac9
Marcus <mv_bcardoso@outlook.com>
2023-03-27 14:44:50 +0100

    editing credits

--- resources/views/user_template/credits.blade.php
+++ resources/views/user_template/credits.blade.php
@@ -1,7 +1,28 @@
 @extends('user_template.layouts.template')
 @section('main-content')
-<!-- fashion section start -->
 <div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="box_main">
+                <div>
+                <h2>Fundador Dragon Component<h2>
+                </div>
+                    <ul>
+                        <li>Marcus Vinicius Batista Cardoso</li>
+                        <li>21 Anos</li>
+                        <li>Sonho em ser Técnico de CyberSegurança</li>
+                    </ul>
+            </div>
+        </div>
+            <div class="col-lg-8">
+                <div class="box_main">
+                        <h3>Tenho orgulho de poder estar realizando este projeto para que com isso me torne alguem melhor,
+                            assim podendo dar um sentimento de gratidão a minha familia por ver que tem alguém que realmente
+                            se esforça em tudo.
 
+                            Graças a Deus tenho tudo aquilo que preciso para conseguir ! Amem<h3>
+                </div>
+            </div>
+    </div>
 </div>
 @endsection
