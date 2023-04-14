f2bf16dc8c58b5accf2c2ee1d73ca6e9d3c3050e
Davi Neves <tio.sam989@hotmail.com>
2023-04-14 05:22:21 +0100

    fixing home btn link

--- resources/views/admin/layouts/template.blade.php
+++ resources/views/admin/layouts/template.blade.php
@@ -62,7 +62,7 @@ l
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
+            <a href="/" class="app-brand-link">
 
               <span class="app-brand-text demo menu-text fw-bolder ms-2">DGComponent</span>
             </a>
