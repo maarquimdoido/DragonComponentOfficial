59e01de31f2dcd8fb8a73be2a47d06f88e13919d
maarquimdoido <mv_bcardoso@outlook.com>
2023-04-19 09:34:40 +0100

    checking migrations

--- database/migrations/2023_03_24_011341_create_orders_table.php
+++ database/migrations/2023_03_24_011341_create_orders_table.php
@@ -1,4 +1,4 @@
-<?php
+'<?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('product_name');
             $table->integer('quantity');
             $table->integer('total_price');
+            $table->string('status');
             $table->timestamps();
         });
     }
