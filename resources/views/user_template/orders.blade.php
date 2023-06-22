80e7887e92ada1049b194ba640154119d195cccc
Davi Neves <tio.sam989@hotmail.com>
2023-06-22 23:02:28 +0100

    fix order

--- resources/views/user_template/orders.blade.php
+++ resources/views/user_template/orders.blade.php
@@ -1,39 +1,41 @@
 @extends('user_template.layouts.user_profile_template')
 @section('profilecontent')
 <h2>Confirmed Orders</h2>
-    @if($confirmed_orders->isEmpty())
-    <div class="text-center">
-        Empty
-    </div>
-    @else
+@if($confirmed_orders->isEmpty())
+<div class="text-center">
+    Empty
+</div>
+@else
 
-    <table class="table">
-        <tr>
-            <th class="text-center">User ID</th>
-            <th class="text-center">Order Ref.</th>
-            <th class="text-center">Product</th>
-            <th class="text-center">Price</th>
-            <th class="text-center">Status</th>
-        </tr>
-        @foreach($confirmed_orders as $order )
-        <tr>
-            <td class="text-center">
-                {{ $order->userid }}
-            </td>
-            <td class="text-center">
-                {{ $order->id }}
-            </td>
-            <td class="text-center">
-                {{ $order->product_name }}
-                </td>
-            <td class="text-center">
+<table class="table">
+    <tr>
+        <th class="text-center">User ID</th>
+        <th class="text-center">Order Ref.</th>
+        <th class="text-center">Product</th>
+        <th class="text-center">Price</th>
+        <th class="text-center">Status</th>
+    </tr>
+    @foreach($confirmed_orders as $order )
+
+    <input hidden="text" value="{{$order->userid}}" name="userid">
+    <tr>
+        <td class="text-center">
+            {{ $order->userid }}
+        </td>
+        <td class="text-center">
+            {{ $order->id }}
+        </td>
+        <td class="text-center">
+            {{ $order->product_name }}
+        </td>
+        <td class="text-center">
             {{ $order->total_price }}€
-            </td>
-            <td>
-            <input type="button" class="btn btn-success disabled" value="{{ $order->status }}"></input>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    @endif
+        </td>
+        <td>
+            <input type="button" class="btn btn-success disabled" value="{{ $order->status }}" />
+        </td>
+    </tr>
+    @endforeach
+</table>
+@endif
 @endsection
