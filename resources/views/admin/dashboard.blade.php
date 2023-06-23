3d2a0be5898a3328d88b0cf7bfbd4d6f9fe7d2c4
Davi Neves <tio.sam989@hotmail.com>
2023-06-23 11:46:45 +0100

    trying to cresate something

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,212 +4,191 @@ Dashboard - Dragon Component
 @endsection
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
-            <div class="card">
-                <h5 class="card-header">All users - ADMIN</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead class="table-light">
-                      <tr>
-                        <th>ALL ACCOUNTS</th>
-                        <th>VERIFIED ACCOUNTS</th>
-                        <th>UNVERIFIED ACCOUNTS</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>Meh €</td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>Meh €</td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>Meh €</td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+
+  <div class="card flex flex-row">
+    <div class="card flex flex-col" >
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+    </div>
+    <div class="card flex-col" >
+      <div>b</div>
+      <div>b</div>
+      <div>b</div>
+    </div>
+  </div>
+  
+  <div class="card my-4">
+    <h5 class="card-header">Meh title</h5>
+    <div class="card-responsive text-nowrap">
+      <table class="table">
+
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+  </div>
+
+  <div class="card">
+    <h5 class="card-header">All users - ADMIN</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  
+  <br><br>
+  <div class="card">
+    <h5 class="card-header">Availabe Products Information</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th>ID</th>
+            <th>Product Name</th>
+            <th>Image</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+            <td>Meh</td>
+            <td>
+              <img style="height: 100px;"
+                src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
+            </td>
+            <td>Meh €</td>
+            <td>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
+            </td>
+          </tr>
+          <tr>
+            <div class="dropdown">
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
               </div>
-              <br><br>
-            <div class="card">
-                <h5 class="card-header">Availabe Products Information</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead class="table-light">
-                      <tr>
-                        <th>ID</th>
-                        <th>Product Name</th>
-                        <th>Image</th>
-                        <th>Price</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>
-                          <img  style="height: 100px;"src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
-                        </td>
-                        <td>Meh €</td>
-                        <td>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
-                        </td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+            </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <br><br>
+  <div class="card">
+    <h5 class="card-header">Availabe Products Information</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th>ID</th>
+            <th>Product Name</th>
+            <th>Image</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+            <td>Meh</td>
+            <td>
+              <img style="height: 100px;"
+                src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
+            </td>
+            <td>Meh €</td>
+            <td>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
+            </td>
+          </tr>
+          <tr>
+            <div class="dropdown">
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
               </div>
-              <br><br>
-            <div class="card">
-                <h5 class="card-header">Availabe Products Information</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead class="table-light">
-                      <tr>
-                        <th>ID</th>
-                        <th>Product Name</th>
-                        <th>Image</th>
-                        <th>Price</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>
-                          <img  style="height: 100px;"src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
-                        </td>
-                        <td>Meh €</td>
-                        <td>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
-                        </td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+            </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <br><br>
+  <div class="card">
+    <h5 class="card-header">Availabe Products Information</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th>ID</th>
+            <th>Product Name</th>
+            <th>Image</th>
+            <th>Price</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          <tr>
+            <td>Meh</td>
+            <td>Meh</td>
+            <td>
+              <img style="height: 100px;"
+                src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
+            </td>
+            <td>Meh €</td>
+            <td>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
+              <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
+            </td>
+          </tr>
+          <tr>
+            <div class="dropdown">
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
               </div>
-              <br><br>
-            <div class="card">
-                <h5 class="card-header">Availabe Products Information</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead class="table-light">
-                      <tr>
-                        <th>ID</th>
-                        <th>Product Name</th>
-                        <th>Image</th>
-                        <th>Price</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td>Meh</td>
-                        <td>Meh</td>
-                        <td>
-                          <img  style="height: 100px;"src="https://i.pinimg.com/originals/88/eb/00/88eb008794bf5dde8c0eab80662d446f.png" alt="">
-                        </td>
-                        <td>Meh €</td>
-                        <td>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Edit</a>
-                            <a href="#" class="btn text-white" style="background-color:#dc3545;">Delete</a>
-                        </td>
-                      </tr>
-                      <tr>
-                          <div class="dropdown">
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
-                            </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              
+            </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 </div>
 @endsection
